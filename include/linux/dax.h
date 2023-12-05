f8380f59cb91ae42268af3414cbabcb9e333a92c
John Groves <john@groves.net>
2023-12-05 10:43:26 -0600

    dev_dax_iomap: Add fs_dax_get() func to prepare dax for fs-dax usage
    This function should be called by fs-dax file systems after opening the
    devdax device. This adds holder_operations, which effects exclusivity
    between callers of fs_dax_get().
    
    This function serves the same role as fs_dax_get_by_bdev(), which dax
    file systems call after opening the pmem block device.
    
    This also adds the CONFIG_DEV_DAX_IOMAP Kconfig parameter

--- include/linux/dax.h
+++ include/linux/dax.h
@@ -57,6 +57,11 @@ struct dax_holder_operations {
 
 #if IS_ENABLED(CONFIG_DAX)
 struct dax_device *alloc_dax(void *private, const struct dax_operations *ops);
+
+#if IS_ENABLED(CONFIG_DEV_DAX_IOMAP)
+int fs_dax_get(struct dax_device *dax_dev, void *holder, const struct dax_holder_operations *hops);
+struct dax_device *inode_dax(struct inode *inode);
+#endif
 void *dax_holder(struct dax_device *dax_dev);
 void put_dax(struct dax_device *dax_dev);
 void kill_dax(struct dax_device *dax_dev);
