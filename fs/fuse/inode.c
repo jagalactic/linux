f30d833a7063d5bb713a39710a7ed82bdf791cee
John Groves <john@groves.net>
2024-07-26 10:25:39 -0500

    famfs_fuse: GET_DAXDEV message and daxdev_table
    * The new GET_DAXDEV message/response is enabled
    * The command it triggered by the update_daxdev_table() call, if there
      are any daxdevs in the subject fmap that are not represented in the
      daxdev_dable yet.

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -1049,8 +1049,10 @@ void fuse_conn_put(struct fuse_conn *fc)
 		}
 		if (IS_ENABLED(CONFIG_FUSE_PASSTHROUGH))
 			fuse_backing_files_free(fc);
-		if (IS_ENABLED(CONFIG_FUSE_FAMFS_DAX))
+		if (IS_ENABLED(CONFIG_FUSE_FAMFS_DAX)) {
 			kfree(fc->shadow);
+			famfs_teardown(fc);
+		}
 		call_rcu(&fc->rcu, delayed_release);
 	}
 }
