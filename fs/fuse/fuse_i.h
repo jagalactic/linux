c335361407862d364d18f776cd2b7c1161a2f2f9
John Groves <john@groves.net>
2024-07-26 10:25:39 -0500

    famfs_fuse: Basic fuse kernel ABI enablement for famfs
    * FUSE_DAX_FMAP flag in INIT request/reply
    
    * fuse_conn->famfs_iomap (enable famfs-mapped files) to denote a
      famfs-enabled connection

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -870,6 +870,9 @@ struct fuse_conn {
 	/* Use pages instead of pointer for kernel I/O */
 	unsigned int use_pages_for_kvec_io:1;
 
+	/* dev_dax_iomap support for famfs */
+	unsigned int famfs_iomap:1;
+
 	/* Use io_uring for communication */
 	unsigned int io_uring;
 
