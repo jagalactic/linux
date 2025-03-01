1382eaa10afcbc0eb3eb8f8f109647bcbb6d7ab0
John Groves <john@groves.net>
2025-03-01 10:03:58 -0600

    famfs_fuse: magic.h: Add famfs magic numbers
    Famfs distinguishes between its on-media and in-memory superblocks

--- include/uapi/linux/magic.h
+++ include/uapi/linux/magic.h
@@ -38,6 +38,8 @@
 #define OVERLAYFS_SUPER_MAGIC	0x794c7630
 #define FUSE_SUPER_MAGIC	0x65735546
 #define BCACHEFS_SUPER_MAGIC	0xca451a4e
+#define FAMFS_SUPER_MAGIC	0x87b282ff
+#define FAMFS_STATFS_MAGIC      0x87b282fd
 
 #define MINIX_SUPER_MAGIC	0x137F		/* minix v1 fs, 14 char names */
 #define MINIX_SUPER_MAGIC2	0x138F		/* minix v1 fs, 30 char names */
