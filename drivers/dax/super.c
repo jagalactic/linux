847ecd4cbdd19386498e30fce48b2001e7afe6d5
John Groves <John@Groves.net>
2026-05-19 08:59:14 -0500

    dax: Remove unused export of dax_dev_get()
    Commit 2ae624d5a555 ("dax: export dax_dev_get()") exported dax_dev_get()
    in v7.2 for famfs, which has not merged. The export has never had an
    in-tree caller, so make dax_dev_get() static again.
    
    Signed-off-by: John Groves <John@Groves.net>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Alison Schofield <alison.schofield@intel.com>

--- drivers/dax/super.c
+++ drivers/dax/super.c
@@ -521,7 +521,7 @@ static int dax_set(struct inode *inode, void *data)
 	return 0;
 }
 
-struct dax_device *dax_dev_get(dev_t devt)
+static struct dax_device *dax_dev_get(dev_t devt)
 {
 	struct dax_device *dax_dev;
 	struct inode *inode;
@@ -544,7 +544,6 @@ struct dax_device *dax_dev_get(dev_t devt)
 
 	return dax_dev;
 }
-EXPORT_SYMBOL_GPL(dax_dev_get);
 
 struct dax_device *alloc_dax(void *private, const struct dax_operations *ops)
 {
